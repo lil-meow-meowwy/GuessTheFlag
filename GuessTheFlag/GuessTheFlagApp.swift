18650c0dcc1ac5f31e59f66573622339b4bf613d
Kristina Yaroshenko <kris.jaroshenko09@gmail.com>
2025-05-13 18:39:25 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kristina Yaroshenko on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessTheFlagTests/GuessTheFlagTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagTests.swift
+//  GuessTheFlagTests
+//
+//  Created by Kristina Yaroshenko on 13.05.2025.
+//
+
+import Testing
+@testable import GuessTheFlag
+
+struct GuessTheFlagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
